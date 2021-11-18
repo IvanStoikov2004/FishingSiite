cef63ec8502cab3feedd34c30daea44f6186a32f
Ivanstoikov123456 <ivan13.is@abv.bg>
2021-11-18 21:24:38 +0200

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,24 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
     }
 }
